2805e18e66ad4682b09f93a1c73bd74ea4337bba
Tomas <tomasysh@gmail.com>
2022-03-29 00:23:34 +0800

    8. [Manipulating Elements] Creating New Elements ($(htmlTag) )

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
-    const $myList = $('.myList').click(function() {
-        console.log('click #myList');
-    });
+    $(`<h1>HELLO WORLD</h1>
+        <h1>jQuery Playbook</h1>
+        <span>20220330-20220331</span>
+        <span>TomasYSH</span>`).prependTo('body');
 
-    const $cloneList = 
-        $myList
-            .clone(true)
-            .appendTo('body');
+    $('<a>', {
+        html: `Text me`,
+        href: 'mailto: tomasysh@gmail.com',
+        target: '_blank',
+    }).insertBefore('.myList');
 
 });
