7d5f5e409402de4559bbdbbebfec8eced7503ac9
Tomas <tomasysh@gmail.com>
2022-03-28 22:07:19 +0800

    5. [Utility Methods] $.type()

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-
-    const students = ['amy', 'sophia', 'tomas'];
-
-    if ($.inArray('sophia', students) !== -1) {
-        console.log('sophia is my student.')
-    }
+    
+    console.log(`Is [] array ? ${$.isArray([])}`);
+    console.log(`Is function() {} a function ? ${$.isFunction(function() {})}`);
+    console.log(`Is 3.14 a number ? ${$.isNumeric(3.14)}`);
+    console.log(`true is a  ${$.type(true)}.`)
+    console.log(`3 is a ${$.type(3)}.`);
+    console.log(`'test' is a ${$.type('test')}.`);
+    console.log(`function() {} is a ${$.type(function() {})}.`);
+    console.log(`new Boolean() is a ${$.type(new Boolean())}.`);
+    console.log(`new Number(3) is a ${$.type(new Number(3))}.`)
+    console.log(`new Function() is a ${$.type(new Function(3))}.`)
+    console.log(`[]  is a ${$.type([])}.`)
+    console.log(`null  is a ${$.type(null)}.`)
+    console.log(`/test/ is a ${$.type(/test/)}.`)
+    console.log(`new Date() is a ${$.type(new Date())}.`)
     
 });
