99464544a65b48e2ffc9b25ba0eb4aeb5d601485
Tomas <tomasysh@gmail.com>
2022-03-29 01:11:47 +0800

    10. [Events I] .hover()

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
 
-    $('button.sad').on('click', function() {
-        alert('so sad');
+    $('.header')
+        .mouseenter(function() {
+            $(this).stop().addClass('highlight').animate({ fontSize: '+=50'}, 1000);
+        })
+        .mouseleave(function() {
+            $(this).stop().removeClass('highlight').animate({ fontSize: '-=50'}, 1000);
+        })
+
+    $('.main').hover(function() {
+        $(this).toggleClass('highlight');
     });
-
-    $(`<button type="button" class="sad">:(</button>`)
-        .appendTo('.header');
-
-    $('.main').on('click', 'button.smile', function() {
-        alert('so happy');
-    })
-
-    $(`<button type="button" class="smile">:)</button>`)
-        .appendTo('.main');
 
 });
