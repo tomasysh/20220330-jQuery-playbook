cb233b29e1fd877531928ed074da296db14f75f5
Tomas <tomasysh@gmail.com>
2022-03-29 07:03:00 +0800

    tips

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    $('h1').fadeTo(3000, 0.1);
+
+    const imageSources = [
+        'https://shieldbroblog.files.wordpress.com/2018/03/20184.jpg',
+        'https://vignette.wikia.nocookie.net/shigatsu-wa-kimi-no-uso/images/f/f0/Kousei_Arima.png/revision/latest?cb=20150209172245',
+        'https://vignette.wikia.nocookie.net/shigatsu-wa-kimi-no-uso/images/6/62/Tsubaki_Sawabe.png/revision/latest?cb=20141210140220',
+        'https://vignette.wikia.nocookie.net/shigatsu-wa-kimi-no-uso/images/a/a5/Kaori_Miyazono.png/revision/latest/scale-to-width-down/300?cb=20141210125454',
+        'https://vignette.wikia.nocookie.net/shigatsu-wa-kimi-no-uso/images/8/8f/Ryota_Watari.png/revision/latest?cb=20150209172401'
+    ];
+   
+   /**
+    * TODO: 設置輪播器初始圖片
+    * tips: 找到 <img> 並使用 .prop() 設定 src 屬性。
+    */
+
+   /**
+    * TODO: 每 3000 秒切換一次圖片
+    * tips: 利用 js modulo(%) 算餘數, 得出切換圖片的 index
+    *       搭配 js 方法 setInterval
+    */
+
+   /**
+    * TODO: (挑戰) 製作 pause/start 按鈕
+    * tips: 取消/再次建立 interval>
+    */
+    
 });
