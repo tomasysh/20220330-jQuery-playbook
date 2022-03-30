f9d1459afa3411c874131089e0c08cf1ddb06a5f
Tomas <tomasysh@gmail.com>
2022-03-31 04:21:30 +0800

    jQuery .ajax() pattern

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {
-    $('h1').fadeTo(3000, 0.1);
-});
+$(document).ready(function () {
+    $('.fetch-data').click(function () {
+      $(this).prop({ disabled: true });
+      $('#pokemon').empty();
+      $('body').addClass('backdrop');
+      setTimeout(() => {
+        getPokemon();
+      }, 1000);
+    });
+
+    function getPokemon() {
+      const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+      $.ajax({
+        url: POKE_API_URL,
+        data: {
+          limit: 10,
+          offset: 0,
+        },
+        type: 'GET',
+        dataType: 'json',
+      })
+        .done(function (response) {
+          console.log('response', response);
+        })
+        .fail(function (error) {
+          console.log('some error occurred :(');
+        })
+        .always(function () {
+          $('body').removeClass('backdrop');
+        });
+    }
+  });
