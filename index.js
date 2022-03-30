fe5765d6eef5ebb381ff16c9548cbd5acfbe7b65
Tomas <tomasysh@gmail.com>
2022-03-31 06:53:35 +0800

    tips

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('h1').fadeTo(3000, 0.1);
+    // TODO: 使用 jQuery events, 監聽 click 事件, 取得 input 的值
+
+    // TODO: 監聽 form 標籤的 submit 事件，console.log 出目前的表單資料
+    //       記得阻止瀏覽器預設的 submit 方法（preventDefault()）
+
+    // TODO: （挑戰）設定 input 的檢核條件, 並在非法(invalid)時，在畫面上做出提示
 });
